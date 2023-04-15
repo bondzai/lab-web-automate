66e1d88f57f3bdc43d8517dff86befab630980e2
JamesBond <introbond.dev@gmail.com>
2023-04-15 19:39:33 +0700

    get raw data + express API

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-const puppeteer = require('puppeteer');
+const app = require('./app');
+const PORT = process.env.PORT || 4001;
 
-const getAllData = async () => {
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
-    await page.goto('https://debank.com/profile/0x1c45e086ed143aef83c1209521a2ff5369f39abc');
-
-    const element = await page.waitForSelector(
-        '#Overview_defiItem__1e5s9 > div:nth-child(2)'
-    );
-
-    const rawData = await element.evaluate(el => el.textContent);
-    let result = rawData.split(" ")
-    console.log(result)
-
-    await browser.close();
-};
-
-getAllData()
+app.listen(PORT, () => {
+    console.log(`server is running at ${PORT}`)
+})
