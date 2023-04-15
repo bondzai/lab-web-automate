7338cd9e372ef4c349a2cefa8c26c0a38e2a301c
JamesBond <introbond.dev@gmail.com>
2023-04-15 16:36:41 +0700

    get raw data

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const puppeteer = require('puppeteer');
 
 const getAllData = async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.goto('https://debank.com/profile/0x1c45e086ed143aef83c1209521a2ff5369f39abc');
 
     const element = await page.waitForSelector(
-        'div > .Project_portfolioProject__2f0GB'
+        '#Overview_defiItem__1e5s9 > div:nth-child(2)'
     );
 
-    const result = await element.evaluate(el => el.textContent);
+    const rawData = await element.evaluate(el => el.textContent);
+    let result = rawData.split(" ")
     console.log(result)
 
     await browser.close();
