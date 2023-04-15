2cb47e65962a12486541ea343d0ca89d538ff113
JamesBond <introbond.dev@gmail.com>
2023-04-15 20:28:03 +0700

    puppeteer config cjs

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,8 +1,10 @@
+const {join} = require('path');
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
+    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
     experiments: {
-      macArmChromiumEnabled: true,
+        macArmChromiumEnabled: true,
     },
-  };
+};
