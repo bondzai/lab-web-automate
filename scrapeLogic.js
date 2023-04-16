8cea0f394641678f53f30222ce5c0fe5981c1806
JamesBond <introbond.dev@gmail.com>
2023-04-16 12:36:50 +0700

    puppeteer stable

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -2,14 +2,14 @@ const puppeteer = require("puppeteer");
 
 const scrape = async (iURL, iSelectedElement) => {
     try {
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         await page.goto(iURL);
         const element = await page.waitForSelector(iSelectedElement);
         const rawData = await element.evaluate(el => el.textContent);
         let result = rawData.split(" ");
         await browser.close();
-        return result
+        return result;
     } catch (e) {
         console.error(e);
         return `Error: ${e}`;
@@ -21,13 +21,12 @@ const scrapeDebank = async (req, res) => {
     const baseURL = `https://debank.com/profile/0x1c45e086ed143aef83c1209521a2ff5369f39abc`
     let fullURL = baseURL
     if (chain) {
-        fullURL = `${baseURL}?chain${chain}`
+        fullURL = `${baseURL}?chain=${chain}`
     }
+    let index = 2;
+    let element = `#Overview_defiItem__1e5s9 > div:nth-child(${index})`
 
-    let result = await scrape(
-        fullURL,
-        '#Overview_defiItem__1e5s9 > div:nth-child(3)'
-    );
+    let result = await scrape(fullURL, element);
     res.json(result);
 };
 
