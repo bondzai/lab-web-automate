4d991eebf7f19576d5be70174cc24fe9c7145535
JamesBond <introbond.dev@gmail.com>
2023-04-16 17:47:28 +0700

    split services folder

--- services/scrapeLogic.js
+++ services/scrapeLogic.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const { map } = require("../app");
 
 const scrape = async (iURL, iSelectedElement) => {
     try {
@@ -16,6 +17,43 @@ const scrape = async (iURL, iSelectedElement) => {
     }
 };
 
+const getNumber = (iData) => {
+    const regex = /\d+\.\d+/;
+    let result = iData.match(regex)[0];
+    console.log(result)
+    return result
+};
+
+const mapData = (iData) => {
+    const result = {}
+    let title = iData[0].split('$')
+    title = title[0]
+    result.title = title
+    const keys = []
+    const values = []
+
+    values.push(getNumber(iData[1]))
+
+    for (const i in iData) {
+        console.log(iData[i])
+        if (i > 1) {
+            if (Number(i) % 2 === 0) {
+                keys.push(iData[i])
+            } else {
+                values.push(iData[i])
+            }
+        }
+    }
+
+    for (const i in keys) {
+        if (keys[i]) {
+            result[keys[i]] = values[i]
+        }
+    }
+
+    console.log(result)
+};
+
 const scrapeDebank = async (req, res) => {
     const { chain } = req.query
     const baseURL = `https://debank.com/profile/0x1c45e086ed143aef83c1209521a2ff5369f39abc`
@@ -27,6 +65,9 @@ const scrapeDebank = async (req, res) => {
     let element = `#Overview_defiItem__1e5s9 > div:nth-child(${index})`
 
     let result = await scrape(fullURL, element);
+    
+    mapData(result)
+
     res.json(result);
 };
 
