6547295bf85082c7ddbbd115db76d2abde603fd6
JamesBond <introbond.dev@gmail.com>
2023-04-16 07:45:51 +0700

    puppeteer test

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,40 +1,33 @@
 require("dotenv").config();
 const puppeteer = require("puppeteer");
 
-const getAllData = async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://debank.com/profile/0x1c45e086ed143aef83c1209521a2ff5369f39abc?chain=arb');
-
-    const element = await page.waitForSelector(
-        '#Overview_defiItem__1e5s9 > div:nth-child(2)'
-    );
-
-    const rawData = await element.evaluate(el => el.textContent);
-    let result = rawData.split(" ");
-    await browser.close();
-    return result
-};
-
-
 const scrapeLogic = async (res) => {
-    const browser = await puppeteer.launch({
-        args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--single-process",
-            "--no-zygote",
-        ],
-        executablePath:
-            process.env.NODE_ENV === "production"
-                ? process.env.PUPPETEER_EXECUTABLE_PATH
-                : puppeteer.executablePath(),
-    });
     try {
-        let x = await getAllData()
-        console.log(x)
+        var browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote",
+            ],
+            executablePath:
+                process.env.NODE_ENV === "production"
+                    ? process.env.PUPPETEER_EXECUTABLE_PATH
+                    : puppeteer.executablePath(),
+        });
+        const page = await browser.newPage();
+        await page.goto('https://debank.com/profile/0x1c45e086ed143aef83c1209521a2ff5369f39abc?chain=arb');
 
-        res.send(x);
+        const element = await page.waitForSelector(
+            '.Project_portfolioProject__2f0GB'
+            // '#Overview_defiItem__1e5s9 > div:nth-child(2)'
+        );
+
+        const rawData = await element.evaluate(el => el.textContent);
+        let result = rawData.split(" ");
+
+        res.send(result);
     } catch (e) {
         console.error(e);
         res.send(`Something went wrong while running Puppeteer: ${e}`);
