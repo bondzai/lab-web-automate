3293b0e3084fc3e4acb5f415e07cc1615e6fd166
JamesBond <introbond.dev@gmail.com>
2023-05-04 12:20:56 +0700

    change local port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
     console.log(`server is running at ${PORT}`)
