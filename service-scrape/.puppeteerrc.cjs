393b27fc4cd3dad3f30d0d47f4d77be9ff0870c6
JamesBond <introbond.dev@gmail.com>
2023-05-09 19:14:24 +0700

    Update: services structure

--- service-scrape/.puppeteerrc.cjs
+++ service-scrape/.puppeteerrc.cjs
@@ -1,7 +1,7 @@
 const {join} = require('path');
 
 module.exports = {
-    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+    cacheDirectory: join(__dirname, '/.cache', 'puppeteer'),
     experiments: {
         macArmChromiumEnabled: true,
     },
